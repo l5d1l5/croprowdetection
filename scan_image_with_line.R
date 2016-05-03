c67a0b2426b50ff1cdc4686361c7f8e950920992
darellvdv <darellvdv@gmail.com>
2016-05-03 11:23:42 +0200

    updated elide function

--- scan_image_with_line.R
+++ scan_image_with_line.R
@@ -63,8 +63,197 @@ anglecalc <- function(numpy_array){
   rad2deg <- function(rad) {(rad * 180) / (pi)}
   angle <- rad2deg(radconv)
   
-  return(angle)
+  list <- list('Rads' = radconv, 'Degree' = angle)
+  
+  return(list)
 }
+
+## Set parameters ##
+line_length <- 120;
+angle <- as.numeric(anglecalc(linesNptxt)[1])
+epsg <- crs(ndvi)
+line_amount <- 250
+spacing <- 0.045
+
+#################
+## CREATE GRID ##
+#################
+
+# Create grid function
+gridcreate <- function(rasterextent, epsg, line_amount, line_length, angle, spacing) {
+  
+  #initialize list to to 2 empty matrices of 2 by 3
+  clist <-  matrix(NA, nrow=line_amount, ncol=2)
+  names(clist) <- c('x','y')
+  
+  # Rad 2 degreee
+  rad2deg <- function(rad) {(rad * 180) / (pi)}
+  angledeg <- rad2deg(angle)
+  angledegsup <- 180 + angledeg
+  
+  # Generate begin coords based on degree
+  if(angledegsup <= 210 & angledegsup >= 150){
+    
+    # Create y points
+    for(i in 1:line_amount){
+      clist[[i,2]]<- (ymin(ndvi) + (i*spacing))
+    }
+    # Create x values
+    for (i in 1:line_amount){
+      clist[[i,1]] <- xmin(ndvi) 
+    }} else {
+      
+      # Create x values
+      for(i in 1:line_amount){
+        clist[[i,2]]<- ymin(ndvi)
+      }
+      # Create y values
+      for (i in 1:line_amount){
+        clist[[i,1]] <- (xmin(ndvi) + (i*spacing))
+      }
+    }
+
+  # Generate line
+  x1 = clist[,1]
+  y1 = clist[,2]
+  x2 = x1 + line_length * cos(angle)
+  y2 = y1 + line_length * sin(angle)
+
+  begin.coord <- cbind(x1, y1)
+  end.coord <- cbind(x2, y2)
+  
+  points <- rbind(begin.coord, end.coord)
+  colnames(points) <- c('x','y')
+  points <- transform(points, id = as.numeric(interaction(x, y, drop=TRUE)))
+  
+  ## raw list to store Lines objects
+  rawlist <- vector("list", nrow(begin.coord))
+  
+  for (i in seq_along(rawlist)) {
+    rawlist[[i]] <- Lines(list(Line(rbind(begin.coord[i, ], end.coord[i,]))), as.character(i))
+  }
+  
+  splines <- SpatialLines(rawlist, proj4string = epsg)
+  
+  # set rotatations for lines
+  #rotatelist <- list(-1,-0.5,0,0.5,1)
+  
+  # Rotate spatial lines and unlist
+  #gridElide <- lapply(rotatelist, function(x) elide(splines, rotate = x, center=apply(bbox(splines), 1, mean)))
+  
+  #ll0 <- lapply(gridElide , function(x) `@`(x , "lines"))
+  #ll1 <- lapply( unlist( ll0 ) , function(y) `@`(y,"Lines"))
+  #Sl <- SpatialLines( list( Lines( unlist( ll1 ) , ID = 1 ))) # ERROR: makes 1 feature of gridElide
+  #proj4string(Sl) <- epsg
+  
+  
+  # Extract NDVI values
+  #gridVal <- lapply(gridElide, function(x) extract(ndvi, x, method = 'simple', fun = 'sum', na.rm = TRUE, df = TRUE, sp = FALSE))
+  
+  data <- points[1:line_amount,]
+  splinesdf <- SpatialLinesDataFrame(splines, data, match.ID = FALSE)
+  
+  splinesdfVal <- extract(ndvi, splinesdf, method = 'simple', fun = 'sum', na.rm = TRUE, df = TRUE, sp = TRUE)
+  
+  return(splinesdfVal)
+}
+
+system.time(grid <- gridcreate(ndvi, epsg, line_amount, line_length, angle, spacing))
+
+# write a shapefile
+writeOGR(grid, getwd(),
+         "temp/scanlines_splinesdfVal", driver="ESRI Shapefile")
+
+
+# Subset grid based on high NDVI values (top 10% of data)
+q <- quantile(grid$ndvi, 0.90)
+grid_croprows <- grid[grid$ndvi >= q,]
+
+# write a shapefile
+writeOGR(grid_croprows, getwd(),
+         "temp/scanlines_gridcroprows", driver="ESRI Shapefile")
+
+# Apply elide on crop rows in order to test other angles
+# set rotatations for lines
+rotatelist <- list(-0.2, -0.1, 0.1, 0.2)
+
+# Rotate spatial lines and unlist
+gridElide <- lapply(rotatelist, function(x) elide(grid_croprows, rotate = x, center=apply(bbox(grid_croprows), 1, mean)))
+# Extract ndvi from rotated lines
+gridValdf <- lapply(gridElide, function(x) extract(ndvi, x, method = 'simple', fun = 'sum', na.rm = TRUE, df = TRUE, sp = FALSE))
+gridValsp <- lapply(gridElide, function(x) extract(ndvi, x, method = 'simple', fun = 'sum', na.rm = TRUE, df = TRUE, sp = TRUE))
+
+# Combine ndvi of angles to ndvi of straight line and check highest value
+anglesndvi <- as.data.frame(gridValdf)
+keep <- c('ndvi', 'ndvi.1', 'ndvi.2', 'ndvi.3')
+anglesndvi <- anglesndvi[keep]
+straightndvi <- as.data.frame(grid_croprows$ndvi)
+allanglesndvi <- cbind(straightndvi, anglesndvi)
+colnames(allanglesndvi) <- c('0', '-0.2','-0.1','0.1','0.2')
+
+# Compare rotated angles with grid_croprows
+maxndvi <- colnames(allanglesndvi)[apply(allanglesndvi,1,which.max)]
+allanglesndvi['MaxNDVIangle'] <- maxndvi
+
+croprowone <- elide(grid_croprows[1])
+
+
+# write a shapefile
+writeOGR(grid, getwd(),
+         "temp/scanlines_test_5000_0002", driver="ESRI Shapefile")
+
+# Calculate the number of cores
+#no_cores <- detectCores() - 1
+# Initiate cluster
+#cl <- makeCluster(no_cores)
+#linevalMC <- parLapply(cl, ndvi, fun = extract(ndvi, grid, method = 'simple', fun = 'sum', na.rm = TRUE, df = TRUE))
+
+cropmintwo <- elide(grid_croprows, rotate = -0.2, center=apply(bbox(grid_croprows), 1, mean))
+proj4string(cropmintwo) <- epsg
+cropminone <- elide(grid_croprows, rotate = -0.1, center=apply(bbox(grid_croprows), 1, mean))
+proj4string(cropminone) <- epsg
+
+# write a shapefile
+writeOGR(cropminone, getwd(),
+         "temp/cropminone", driver="ESRI Shapefile")
+
+
+#----------------------------------------------------------#
+rotatelist <- list(-1,-0.5,0,0.5,1)
+
+gridElide <- lapply(rotatelist, function(x) elide(splines, rotate = x, center=apply(bbox(splines), 1, mean)))
+
+#  Get the Lines objects which contain multiple 'lines'
+ll0 <- lapply(gridElide , function(x) `@`(x , "lines") )
+
+#  Extract the individual 'lines'
+ll1 <- lapply( unlist( ll0 ) , function(y) `@`(y,"Lines") )
+
+#  Combine them into a single SpatialLines object
+Sl <- SpatialLines( list( Lines( unlist( ll1 ) , ID = 1)))
+proj4string(Sl) <- epsg
+
+sldf <- SpatialLinesDataFrame(Sl, data = gridValsp, match.id = FALSE)
+
+gridVal <- lapply(gridElide, function(x) extract(ndvi, x, method = 'simple', fun = 'sum', na.rm = TRUE, df = TRUE, sp = TRUE))
+
+# write a shapefile
+writeOGR(gridElide, getwd(),
+         "temp/scanlines_gridElide", driver="ESRI Shapefile")
+
+#-------------------------------------------------------------#
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Set Parameters
 angle <- anglecalc(linesNptxt)
